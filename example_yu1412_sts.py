1d9708d50ad2f9baafaa79383920edfb4e00fbc2
Petr Baudis <pasky@ucw.cz>
2016-01-22 15:44:12 +0100

    example_yu1412_sts.py: Stronger regularization, 300D vectors

--- example_yu1412_sts.py
+++ example_yu1412_sts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-An example sts2015 classifier using the (Yu, 2014) approach
+An example sts2015 classifier using the (Yu, 2014) inspired approach
 http://arxiv.org/abs/1412.1632 of training (M, b) such that:
 
     f(q, a) = sigmoid(q * M * a.T + b)
@@ -9,16 +9,16 @@ We use a trivial keras model to train an embedding projection M
 while using bag-of-words average for the two sentenes.
 
 Prerequisites:
-    * Get glove.6B.50d.txt from http://nlp.stanford.edu/projects/glove/
+    * Get glove.6B.300d.txt from http://nlp.stanford.edu/projects/glove/
 
 Performance (3000 iters):
-    9092/9092 [==============================] - 0s - loss: 1.5190 - acc: 0.3543 - val_loss: 1.9104 - val_acc: 0.1753
-    Train Pearson: 0.655718
-    Train Spearman: 0.586274
-    Train MSE: 1.233807
-    Test Pearson: 0.236014
-    Test Spearman: 0.228541
-    Test MSE: 2.705417
+    9092/9092 [==============================] - 2s - loss: 1.4630 - acc: 0.4040 - val_loss: 2.1378 - val_acc: 0.1737
+    Train Pearson: 0.835212
+    Train Spearman: 0.796621
+    Train MSE: 0.693983
+    Test Pearson: 0.265433
+    Test Spearman: 0.285071
+    Test MSE: 3.056868
 """
 
 from __future__ import print_function
@@ -53,14 +53,14 @@ def load_set(glove, globmask):
     return ([np.array(s0), np.array(s1)], labels)
 
 
-def prep_model(glove, dropout=0.1, l2reg=1e-3):
+def prep_model(glove, dropout=0.3, l2reg=1e-3):
     # XXX: this is a bit hacky to combine dot-product with six-wise output
     model0 = Sequential()  # s0
+    model1 = Sequential()  # s1
     model0.add(Dropout(dropout, input_shape=(glove.N,)))
+    model1.add(Dropout(dropout, input_shape=(glove.N,)))
     model0.add(Dense(input_dim=glove.N, output_dim=glove.N, W_regularizer=l2(l2reg)))  # M matrix
     model0.add(RepeatVector(6))  # [nclass]
-    model1 = Sequential()  # s1
-    model1.add(Dropout(dropout, input_shape=(glove.N,)))
     model1.add(RepeatVector(6))  # [nclass]
 
     model = Sequential()
@@ -80,13 +80,13 @@ def eval_set(model, X, y, name):
 
 
 if __name__ == "__main__":
-    glove = emb.GloVe(N=50)  # for our naive method, 300**2 would be too much
+    glove = emb.GloVe(N=300)
     Xtrain, ytrain = load_set(glove, 'sts/all/201[0-4]*')
     Xtest, ytest = load_set(glove, 'sts/all/2015*')
 
     model = prep_model(glove)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
-    model.fit(Xtrain, loader.sts_labels2categorical(ytrain), batch_size=80, nb_epoch=30, show_accuracy=True,
+    model.fit(Xtrain, loader.sts_labels2categorical(ytrain), batch_size=80, nb_epoch=200, show_accuracy=True,
               validation_data=(Xtest, loader.sts_labels2categorical(ytest)))
     eval_set(model, Xtrain, ytrain, 'Train')
     eval_set(model, Xtest, ytest, 'Test')
