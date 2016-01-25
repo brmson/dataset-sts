4c832a6068bbdf443ddfb014684e1f26c5849f87
Petr Baudis <pasky@ucw.cz>
2016-01-25 10:33:02 +0100

    example_yu1412_sts.py: Add sick2014 option

--- example_yu1412_sts.py
+++ example_yu1412_sts.py
@@ -12,17 +12,27 @@ Prerequisites:
     * Get glove.6B.300d.txt from http://nlp.stanford.edu/projects/glove/
 
 Performance (200 iters):
-    9092/9092 [==============================] - 2s - loss: 1.4630 - acc: 0.4040 - val_loss: 2.1378 - val_acc: 0.1737
-    Train Pearson: 0.835212
-    Train Spearman: 0.796621
-    Train MSE: 0.693983
-    Test Pearson: 0.265433
-    Test Spearman: 0.285071
-    Test MSE: 3.056868
+    * sts (2012-2014 train, 2015 test)
+        9092/9092 [==============================] - 2s - loss: 1.4630 - acc: 0.4040 - val_loss: 2.1378 - val_acc: 0.1737
+        Train Pearson: 0.835212
+        Train Spearman: 0.796621
+        Train MSE: 0.693983
+        Test Pearson: 0.265433
+        Test Spearman: 0.285071
+        Test MSE: 3.056868
+    * sick2014
+        4500/4500 [==============================] - 1s - loss: 1.1657 - acc: 0.5389 - val_loss: 1.2519 - val_acc: 0.4589
+        Train Pearson: 0.871274
+        Train Spearman: 0.807043
+        Train MSE: 0.291123
+        Test Pearson: 0.690318
+        Test Spearman: 0.571486
+        Test MSE: 0.572896
 """
 
 from __future__ import print_function
 
+import argparse
 import glob
 import numpy as np
 from sklearn.metrics import mean_squared_error as mse
@@ -37,8 +47,8 @@ import pysts.embedding as emb
 import pysts.loader as loader
 
 
-def load_set(glove, globmask):
-    s0, s1, labels = loader.concat_datasets([loader.load_sts(d) for d in glob.glob(globmask)])
+def load_set(glove, globmask, loadfun=loader.load_sts):
+    s0, s1, labels = loader.concat_datasets([loadfun(d) for d in glob.glob(globmask)])
     print('(%s) Loaded dataset: %d' % (globmask, len(s0)))
     e0, e1, s0, s1, labels = loader.load_embedded(glove, s0, s1, labels)
     return ([e0, e1], labels)
@@ -71,9 +81,18 @@ def eval_set(model, X, y, name):
 
 
 if __name__ == "__main__":
-    glove = emb.GloVe(N=300)
-    Xtrain, ytrain = load_set(glove, 'sts/all/201[0-4]*')
-    Xtest, ytest = load_set(glove, 'sts/all/2015*')
+    parser = argparse.ArgumentParser(description="Benchmark yu1412 on semantic relatedness regression-classification (sts)")
+    parser.add_argument("-N", help="GloVe dim", type=int, default=300)
+    parser.add_argument("--sick", help="whether to run on SICK2014 inst. of sts2012-14/15 dataset", type=int, default=0)
+    args = parser.parse_args()
+
+    glove = emb.GloVe(N=args.N)
+    if args.sick == 1:
+        Xtrain, ytrain = load_set(glove, 'sick2014/SICK_train.txt', loader.load_sick2014)
+        Xtest, ytest = load_set(glove, 'sick2014/SICK_test_annotated.txt', loader.load_sick2014)
+    else:
+        Xtrain, ytrain = load_set(glove, 'sts/all/201[0-4]*')
+        Xtest, ytest = load_set(glove, 'sts/all/2015*')
 
     model = prep_model(glove)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
