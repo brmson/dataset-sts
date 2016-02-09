750ebfffff43add9ff11c29ae019638a7e9a1bee
Petr Baudis <pasky@ucw.cz>
2016-02-09 04:18:03 +0100

    pysts.kerasts: +STSPerasonCB, graph_input_sts()
    Example to follow soon.

--- pysts/kerasts/__init__.py
+++ pysts/kerasts/__init__.py
@@ -11,3 +11,13 @@ def graph_input_anssel(si0, si1, y, f0=None, f1=None):
         gr['f0'] = f0
         gr['f1'] = f1
     return gr
+
+
+def graph_input_sts(si0, si1, y, f0=None, f1=None):
+    """ Produce Keras task specification from vocab-vectorized sentences. """
+    import pysts.loader as loader
+    gr = {'si0': si0, 'si1': si1, 'classes': loader.sts_labels2categorical(y)}
+    if f0 is not None:
+        gr['f0'] = f0
+        gr['f1'] = f1
+    return gr
--- pysts/kerasts/callbacks.py
+++ pysts/kerasts/callbacks.py
@@ -5,6 +5,7 @@ Task-specific callbacks for the fit() function.
 from keras.callbacks import Callback
 
 import pysts.eval as ev
+import pysts.loader as loader
 
 
 class AnsSelCB(Callback):
@@ -17,3 +18,17 @@ class AnsSelCB(Callback):
         ypred = self.model.predict(self.val_gr)['score'][:,0]
         mrr = ev.mrr(self.val_s0, self.val_gr['score'], ypred)
         print('                                                       val mrr %f' % (mrr,))
+        logs['mrr'] = mrr
+
+
+class STSPearsonCB(Callback):
+    def __init__(self, train_gr, val_gr):
+        self.train_gr = train_gr
+        self.val_gr = val_gr
+    def on_epoch_end(self, epoch, logs={}):
+        prtr = ev.eval_sts(self.model.predict(self.train_gr)['classes'],
+                           loader.sts_categorical2labels(self.train_gr['classes']), 'Train', quiet=True)
+        prval = ev.eval_sts(self.model.predict(self.val_gr)['classes'],
+                            loader.sts_categorical2labels(self.val_gr['classes']), 'Val', quiet=True)
+        print('                  train Pearson %f    val Pearson %f' % (prtr, prval))
+        logs['pearson'] = prval
