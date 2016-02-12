7b9cb62d353e2b503a82fd5f5be1ba5397b04151
Petr Baudis <pasky@ucw.cz>
2016-02-12 02:07:51 +0100

    anssel_cnn: Final parameter tuning on wang dev

--- examples/anssel_cnn.py
+++ examples/anssel_cnn.py
@@ -25,6 +25,7 @@ Prerequisites:
 
 Performance:
     * wang:  (the model parameters were tuned to maximize devMRR on wang)
+      devMRR=0.876154, testMRR=0.820956, testMAP=0.7321
 
 """
 
@@ -69,10 +70,10 @@ def load_set(fname, vocab=None):
     return (s0, s1, y, vocab, gr)
 
 
-def prep_model(glove, vocab, dropout=3/4, dropout_in=None, l2reg=1e-4,
-               cnnact='tanh', cnninit='glorot_uniform', cdim={1: 1/2, 2: 1/2, 3: 1/2, 4: 1/2, 5: 1/2},
+def prep_model(glove, vocab, dropout=1/2, dropout_in=4/5, l2reg=1e-4,
+               cnnact='tanh', cnninit='glorot_uniform', cdim={1: 1, 2: 1/2, 3: 1/2, 4: 1/2, 5: 1/2},
                project=True, pdim=2.5,
-               ptscorer=B.mlp_ptscorer, mlpsum='sum', Ddim=2,
+               ptscorer=B.mlp_ptscorer, mlpsum='sum', Ddim=1,
                oact='sigmoid'):
     model = Graph()
     N = B.embedding(model, glove, vocab, s0pad, s1pad, dropout)
