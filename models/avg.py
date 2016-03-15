021a0b8a0694781f32b67dc99f40261388d55d62
Petr Baudis <pasky@ucw.cz>
2016-03-15 17:34:24 +0100

    models/avg: Switch default ptscorer to MLP

--- models/avg.py
+++ models/avg.py
@@ -6,10 +6,13 @@ http://arxiv.org/abs/1412.1632 of training (M, b) such that:
 
     f(q, a) = sigmoid(q * M * a.T + b)
 
+However, rather than a dot-product, the MLP comparison is used as it works
+dramatically better.
+
 This model can also represent the Deep Averaging Networks
 (http://cs.umd.edu/~miyyer/pubs/2015_acl_dan.pdf) with this configuration:
 
-    inp_e_dropout=0 inp_w_dropout=1/3 deep=2
+    inp_e_dropout=0 inp_w_dropout=1/3 deep=2 "pact='relu'"
 
 The model also supports preprojection of embeddings (not done by default;
 wproj=True), though it doesn't do a lot of good it seems - the idea was to
@@ -18,7 +21,7 @@ allow mixin of NLP flags.
 
 Performance:
     * anssel-yodaqa:
-      valMRR=0.334864
+      valMRR=0.334864 (dot)
 """
 
 from __future__ import print_function
@@ -50,7 +53,7 @@ def config(c):
     c['inp_e_dropout'] = 1/3
     c['inp_w_dropout'] = 0
     # anssel-specific:
-    c['ptscorer'] = B.dot_ptscorer
+    c['ptscorer'] = B.mlp_ptscorer
     c['mlpsum'] = 'sum'
     c['Ddim'] = 1
 
